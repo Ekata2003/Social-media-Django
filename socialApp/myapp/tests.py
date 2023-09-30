83bdee5562a02654b671f94c606635d33d31e152
Prashuu <97186762+prasad-chavan1@users.noreply.github.com>
2023-09-30 08:44:19 +0530

    Update new tests.py

--- socialApp/myapp/tests.py
+++ socialApp/myapp/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+import unittest
+
+# Function to calculate the factorial of a number
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+# Define a test class that inherits from unittest.TestCase
+class TestFactorial(unittest.TestCase):
+
+    def test_factorial_positive(self):
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(5), 120)
+        self.assertEqual(factorial(10), 3628800)
+
+    def test_factorial_negative(self):
+        with self.assertRaises(ValueError):
+            factorial(-1)
+
+if __name__ == '__main__':
+    unittest.main()
 
 # Create your tests here.
